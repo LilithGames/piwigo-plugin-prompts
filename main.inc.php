66202b67858d156ccc4e066588bfe58b80b3c807
hulucc <hulucc@outlook.com>
2023-06-05 11:06:23 +0800

    feat: js

--- main.inc.php
+++ main.inc.php
@@ -7,3 +7,15 @@ Author: hulucc
 */
 
 if (!defined('PHPWG_ROOT_PATH')) die('Hacking attempt!');
+
+add_event_handler('loc_end_picture', 'add_prompts_detail');
+
+function add_prompts_detail() {
+  global $template;
+  $js = '
+  $(document).ready(function() {
+    console.log("hi hulucc");
+  });
+  ';
+  $template->add_footer_script($js, 'SCRIPT');
+}
