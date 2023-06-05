e7ac82d7cc152e95d250c0ed412c59085da73c84
hulucc <hulucc@outlook.com>
2023-06-05 12:14:32 +0800

    feat: block_footer_script

--- main.inc.php
+++ main.inc.php
@@ -8,23 +8,14 @@ Author: hulucc
 
 if (!defined('PHPWG_ROOT_PATH')) die('Hacking attempt!');
 
-function add_js($content, $smarty)
-{
-    $search = '</body>';
-    $replace = '
-        <script>
-            console.log("hulucc");
-        </script>
-    </body>';
-    return str_replace($search, $replace, $content);
-}
-
 
 function add_prompts_detail() {
-  global $template;
-  if ($page['body_id'] == 'thePicturePage') {
-    $template->set_prefilter('picture', 'add_js');
-  }
+    $js = '
+    <script>
+        console.log("hulucc");
+    </script>
+    '
+    return $js
 }
 
-add_event_handler('init', 'add_prompts_detail');
+add_event_handler('block_footer_script', 'add_prompts_detail');
