18b0a1d17a427c97a694ded01fc1729b78c5ac82
hulucc <hulucc@outlook.com>
2023-06-05 11:23:47 +0800

    fix: js

--- main.inc.php
+++ main.inc.php
@@ -10,12 +10,21 @@ if (!defined('PHPWG_ROOT_PATH')) die('Hacking attempt!');
 
 add_event_handler('loc_end_picture', 'add_prompts_detail');
 
+function add_js($content, &$smarty)
+{
+    // 在模板的底部添加 JavaScript 代码
+    $js = '
+    <script>
+        console.log("hulucc");
+    </script>
+    ';
+
+    // 将 JavaScript 代码添加到模板的底部
+    return $content . $js;
+}
+
+
 function add_prompts_detail() {
   global $template;
-  $js = '
-  $(document).ready(function() {
-    console.log("hi hulucc");
-  });
-  ';
-  $template->assign('FOOTER_SCRIPT', $js);
+  $template->set_prefilter('picture', 'add_js');
 }
