e04598ecae24f2ae077c36d33792af24ac88328b
hulucc <hulucc@outlook.com>
2023-06-05 11:29:32 +0800

    feat: init

--- main.inc.php
+++ main.inc.php
@@ -8,8 +8,6 @@ Author: hulucc
 
 if (!defined('PHPWG_ROOT_PATH')) die('Hacking attempt!');
 
-add_event_handler('loc_end_picture', 'add_prompts_detail');
-
 function add_js($content, $smarty)
 {
     $search = '</body>';
@@ -18,7 +16,6 @@ function add_js($content, $smarty)
             console.log("hulucc");
         </script>
     </body>';
-
     return str_replace($search, $replace, $content);
 }
 
@@ -27,3 +24,5 @@ function add_prompts_detail() {
   global $template;
   $template->set_prefilter('picture', 'add_js');
 }
+
+add_event_handler('init', 'add_prompts_detail');
